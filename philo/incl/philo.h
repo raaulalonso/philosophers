137f7ccd0a5bcaecc27ff18833d4904d171ab50b
Raul Alonso Pardo <raalonso@c1r12s1.42madrid.com>
2023-11-20 22:12:48 +0100

    setting_up

---
+++ philo/incl/philo.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raalonso <raalonso@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 21:55:07 by raalonso          #+#    #+#             */
+/*   Updated: 2023/11/20 22:03:32 by raalonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+#endif
